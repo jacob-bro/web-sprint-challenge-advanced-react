e36360fbadf12d7da53451fe0fcfc121eda4fbad
Jacob Brooks <myrefugemyfortress@yahoo.com>
2021-10-10 00:42:12 -0400

    tweaked hook and set up logic in checkout component

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -2,15 +2,12 @@ import React, { useState } from "react";
 
 export const useForm = (initialValue) => {
    const [values, setValues] = useState(initialValue);
+   
 
    const handleChanges = e => {
        setValues({ ...values, [e.target.name]: e.target.value});
    };
 
-   const clearValue = () => {
-       setValues(initialValue);
-   };
-
-   return [values, handleChanges, clearValue];
+   return [values, handleChanges,];
 
 };
